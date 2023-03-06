5940574c60653980e0fa979b84b801adf4afdf21
Murilo Bedim <94548541+MuriloBedim@users.noreply.github.com>
2023-03-06 11:04:10 -0300

    Add files via upload

---
+++ Aula-1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aula-1</title>
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+    <h2><?php echo 5 + 5 ?></h2>
+    <h2><?php echo 5 - 5 ?></h2>
+    <h2><?php echo 5 * 5 ?></h2>
+    <h2><?php echo 5 / 5 ?></h2>
+</body>
+</html>
